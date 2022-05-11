3dc574d4babdb389b2986977af9295b3449478d5
Kim Ward <kmewrd@outlook.com>
2022-05-11 18:10:17 -0500

    Add skeleton of Results component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Form from './components/Form/Form';
+import Results from './components/Results/Results';
 
 const App = () => {
   return (
     <main className="App">
       <h1>Movie Idea Generator</h1>
       <Form />
+      <Results />
     </main>
   );
 }
