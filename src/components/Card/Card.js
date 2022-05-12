87904788f7764752568e72e879e7ba9b34d6a5bb
Kim Ward <kmewrd@outlook.com>
2022-05-11 19:59:27 -0500

    Adjust card styles

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,10 +4,8 @@ import './Card.css';
 const Card = ({ genre, theme, result }) => {
   return (
     <div className='card'>
-      <h3>Genre:</h3>
-      <p>{genre}</p>
-      <h3>Theme:</h3>
-      <p>{theme}</p>
+      <h3>Genre: <span className='unbold'>{genre}</span></h3>
+      <h3>Theme: <span className='unbold'>{theme}</span></h3>
       <h3>Result:</h3>
       <p>{result}</p>
     </div>
