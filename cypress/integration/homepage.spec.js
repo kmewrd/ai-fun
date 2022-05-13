b34ec7a43b8d99e0df687a7c608c908c7d52a4ef
Kim Ward <kmewrd@outlook.com>
2022-05-12 20:03:10 -0500

    Add test for form fields

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -9,4 +9,18 @@ describe('Homepage', () => {
       .get('form')
       .should('be.visible')
   })
+
+  it('should have a way to specify genre and theme to submit', () => {
+    cy.get('form')
+      .should('have.descendants', 'select')
+      .and('have.descendants', 'input')
+
+    cy.get('select[id="genre-select"]')
+      .select('Sci-Fi')
+      .should('have.value', 'Sci-Fi')
+
+    cy.get('input[id="theme-field"]')
+      .type('Mars')
+      .should('have.value', 'Mars')
+  })
 })
