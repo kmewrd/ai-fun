537dada6e4ef3f20e69c1456dcfd09395dfa3e17
Kim Ward <kmewrd@outlook.com>
2022-05-12 20:05:34 -0500

    Add tests checking for Results visibility

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -10,6 +10,18 @@ describe('Homepage', () => {
       .should('be.visible')
   })
 
+  it('should display results underneath the form', () => {
+    cy.get('h2')
+      .contains('Results')
+      .get('section[class="ideas-wrapper"]')
+      .should('be.visible')
+  })
+
+  it('should display a friendly message if no results exist', () => {
+    cy.get('section[class="ideas-wrapper"]')
+      .contains('No ideas to display...yet!')
+  })
+
   it('should have a way to specify genre and theme to submit', () => {
     cy.get('form')
       .should('have.descendants', 'select')
